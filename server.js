baf35f3df366808d86531f2007244b080a65e98d
Sp3c-1997 <info@spectaculis.nl>
2026-03-30 12:40:03 +0200

    Initialize Express server with Stripe integration

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+app.use(express.json());
+
+app.post('/create-session', async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const customer = await stripe.customers.create({
+      email: email,
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      mode: 'setup',
+      customer: customer.id,
+      payment_method_types: ['sepa_debit'],
+      success_url: 'https://yourstore.myshopify.com/pages/success',
+      cancel_url: 'https://yourstore.myshopify.com/pages/cancel',
+    });
+
+    res.json({ url: session.url });
+
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.post('/webhook', (req, res) => {
+  console.log('Webhook ontvangen');
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log('Server draait'));
